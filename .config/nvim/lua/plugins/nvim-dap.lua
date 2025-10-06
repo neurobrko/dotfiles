43a938cdfb2d70623964a6e494cbc625cd9a8b8f
marpauli_cisco <marpauli@cisco.com>
2025-10-06 09:16:43 +0200

    'gcpm' update, nvim dap setup update

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -88,6 +88,20 @@ return {
         ":lua require('dapui').open({reset = true})<cr>",
         { noremap = true, silent = true, desc = "Reset DAP UI" }
       )
+
+      -- Optional: Key mappings for common DAP actions
+      vim.keymap.set("n", "<F5>", function()
+        dap.continue()
+      end)
+      vim.keymap.set("n", "<F10>", function()
+        dap.step_over()
+      end)
+      vim.keymap.set("n", "<F11>", function()
+        dap.step_into()
+      end)
+      vim.keymap.set("n", "<F12>", function()
+        dap.step_out()
+      end)
     end,
   },
 }
